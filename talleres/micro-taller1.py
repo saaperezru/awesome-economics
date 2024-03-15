2abe1ad107a4c16889a5e090d76ad35991a7cf7d
Santi <saaperezru@gmail.com>
2024-03-15 19:11:18 -0400

    Errore en algebr

--- talleres/micro-taller1.py
+++ talleres/micro-taller1.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.1.88"
+__generated_with = "0.2.8"
 app = marimo.App()
 
 
@@ -79,7 +79,7 @@ def __(mo):
         \]
 
         Lo que quiere decir que la cantidad de equilibrio será:
-        
+
         \[
         \begin{equation}
         \begin{split}
@@ -197,28 +197,30 @@ def __(mo):
 def __(d, np, o, p, plt):
     def draw_subsidy(ax):
         ax.plot(o,p, linestyle=(0, (1, 10)), color='r')
-        
+
         ax.plot(o,np.array(p)-30, linestyle='--', marker='o', color='r')
         ax.axvline(160, linestyle=(0, (1, 10)))
         ax.axhline(290, linestyle=(0, (1, 10)))
-        
+
         ax.plot(d,p, linestyle='--', marker='o', color='b')
-        
+
         ax.set_xlim([120,250])
         ax.set_xticks(np.append(ax.get_xticks(),[160,120,240]))
         ax.set_ylim([250,310])
         ax.set_yticks(np.append(ax.get_yticks(),[290,310]))
         ax.set_xlabel('Quantity')
         ax.set_ylabel('Price')
-        
-        
+
+
         ax.axvline(200, linestyle='dotted')
+        ax.axhline(260, linestyle='dotted')
         ax.axhline(270, linestyle='dotted')
-        
+
         ax.axhline(300, linestyle='dotted')
         ax.annotate('q**',xy=(202,252), xytext=(202,252),annotation_clip=False, color='blue')
         ax.annotate('p*',xy=(122,292), xytext=(122,292),annotation_clip=False, color='blue')
         ax.annotate('p**',xy=(122,272), xytext=(122,272),annotation_clip=False, color='blue')
+        ax.annotate('pq*',xy=(122,262), xytext=(122,262),annotation_clip=False, color='blue')
         ax.annotate('q*',xy=(162,252), xytext=(162,252),annotation_clip=False, color='blue')
 
     _, ax2 = plt.subplots(1,1)
@@ -265,21 +267,18 @@ def __(mo):
     mo.md(
         r"""
         ### 1.d.iii. Beneficio para los consumidores
-        
-        Los consumidores se verán beneficiados en la medida que ahorran (dejan de pagar) un excedente superior al que existía en el precio de equilibrio original. Esta diferencia de ahorro se puede calcular con la integral definida:
+
+        Los consumidores se verán beneficiados en la medida que ahorran (dejan de pagar) un excedente superior al que existía en el precio de equilibrio original ($p^*$). Es decir, calculamos el excedente del nuevo precio de equilibrio ($p^{**}$) y le restamos el excedente en el precio de equilibrio original ($p^*$), lo que nos indica el valor que ahorran los consumidores gracias a la existencia del subsidio.
+
+        Esta diferencia de ahorro se puede calcular con la integral definida:
 
         \[
-        \begin{equation}
-        \begin{split}
-        Costo_e &= q^{**} * p_s \\
-        & = 200 * 30.000 \\
-        & = 6.000.000
-        \end{split}
-        \end{equation}
-        \] 
+        \int_{p^{**}}^{p^*} Q^d(p)dp
+        \]
 
-        """
-    )
+        Que se vería así en la gráfica de oferta-demanda:
+
+    """)
     return
 
 
@@ -295,9 +294,56 @@ def __(draw_subsidy, patches, plt):
 
 @app.cell
 def __(mo):
+    mo.md(r"""Si usamos la integral indefinida de $Q^d$ que llamaremos $E^d$:
+
+        \[
+        \begin{equation}
+        \begin{split}
+        E^d(p) &= \int Q^d(p)dp\\
+        & =  \int (m_dp + c_d)dp\\
+        & = \frac{m_dp^2}{2} + c_dp \\
+        & = 740p -  p^2
+        \end{split}
+        \end{equation}
+        \] 
+
+        Podemos entonces calcular el excedente que beneficia a los consumidores gracias al subsidio así:
+
+        \[
+        \begin{equation}
+        \begin{split}
+        \int_{p^{**}}^{p^*} Q^d(p)dp  &= E^d(p^*) - E^d(p^{**}) \\
+        & = 740p^* -{p^*}^2 - 740p^{**} + {p^{**}}^2 \\
+        & = 740*290 - 290^2 - 740*270 + 270^2 \\
+        & = 3600
+        \end{split}
+        \end{equation}
+        \] 
+
+        Lo que también se puede calcular de forma geométrica considerando que el área con burbujas azules en la figura es la suma del rectángulo hasta $q^*$ más el triángulo de ahí hasta $q^{**}$:
+
+        \[
+        (p^*-p^{**})q^* + \frac{(p^*-p^{**})(q^{**}-q^{*})}{2} = \\
+        (p^*-p^{**}) (q^* + \frac{(q^{**}-q^{*})}{2}) =\\
+        (p^*-p^{**}) (q^* + \frac{(q^{**}-q^{*})}{2}) =\\
+        (p^*-p^{**}) (\frac{q^{**}+q^{*}}{2}) =\\
+        (p^*-p^{**}) \frac{(m_op^{**}+m_op^{*}+2c)}{2} = \\
+        (20) \frac{(-2*270-2*290+2*740)}{2} = \\
+        (20) * \frac{(360)}{2} = 3600
+        \] 
+
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
     mo.md(
         r"""
         ### 1.d.iv Beneficio para los oferentes
+
+        Dado que el oferente se beneficia del aumento en ventas que causa el subsidio, podemos seguir el mismo razonmiento del punto anterior y calcular el beneficio de los oferentes encontrando el área bajo la curva de la función de oferta $Q^o(p)$ entre el precio del nuevo equilibrio y el precio al que estarían dispuestos a vender los oferentes la cantidad del equilibrio existente antes del subsidio ($p^{q^{**}}$). Gráficamente esto se vería así:
         """
     )
     return
@@ -314,6 +360,12 @@ def __(draw_subsidy, patches, plt):
 
 
 @app.cell
+def __(mo):
+    mo.md(r"""O lo que es equivalente:""")
+    return
+
+
+@app.cell
 def __(draw_subsidy, patches, plt):
     _, ax6 = plt.subplots(1,1)
     draw_subsidy(ax6)
@@ -325,9 +377,71 @@ def __(draw_subsidy, patches, plt):
 
 @app.cell
 def __(mo):
+    mo.md(r"""
+        Entonces podemos calcular el precio al que estarían dispuestos a vender la cantidad de equilibrio antes del subsidio despejando en la función de oferta:
+
+        \[
+        \begin{equation}
+        \begin{split}
+        Q_s^o(p^{q^{**}}) &= 160\\
+        4p^{q^{**}} - 880 & =  160\\
+        p^{q^{**}} & = \frac{1040}{4}\\
+        & =  260
+        \end{split}
+        \end{equation}
+        \] 
+
+        Luego, si usamos la integral indefinida de $Q^o$ que llamaremos $E^o$:
+
+        \[
+        \begin{equation}
+        \begin{split}
+        E^o(p) &= \int Q^o(p)dp\\
+        & =  \int (m_op + c_o)dp\\
+        & = \frac{m_op^2}{2} + c_op \\
+        & = \frac{4p^2}{2} - 1000p \\
+        & = 2p^2 - 1000p
+        \end{split}
+        \end{equation}
+        \] 
+
+        Podemos entonces calcular el excedente que beneficia a los consumidores gracias al subsidio así:
+
+        \[
+        \begin{equation}
+        \begin{split}
+        \int_{p^{**}}^{p^*} Q^o(p)dp  &= E^o(p^*) - E^o(p^{**}) \\
+        & = 2{p^*}^2 - 1000p^* - 2{p^{**}}^2 + 1000p^{**} \\
+        & = 2*290^2 - 1000*290 - 2*270^2 + 1000*270 \\
+        & = 2400
+        \end{split}
+        \end{equation}
+        \] 
+
+        Lo que también se puede calcular de forma geométrica considerando que el área con burbujas azules en la figura es la suma del rectángulo hasta $q^*$ más el triángulo de ahí hasta $q^{**}$:
+
+        \[
+        (p^*-p^{**})q^* + \frac{(p^*-p^{**})(q^{**}-q^{*})}{2} = \\
+        (p^*-p^{**}) (q^* + \frac{(q^{**}-q^{*})}{2}) =\\
+        (p^*-p^{**}) (q^* + \frac{(q^{**}-q^{*})}{2}) =\\
+        (p^*-p^{**}) (\frac{q^{**}+q^{*}}{2}) =\\
+        (p^*-p^{**}) \frac{(m_op^{**}+m_op^{*}+2c)}{2} = \\
+        (20) * \frac{(4*270+4*290-2*1000)}{2} = \\
+        (20) * \frac{(240)}{2} = 2400
+        \] 
+
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
     mo.md(
         r"""
         ### 1.d.v. El caso del SOAT en Colombia
+
+        En nuestro caso anteriormente desarrollado la demanda era menos elástica que la oferta, razón por la cual al cambio de precio la demanda no incremento mucho sus cantidades. Este es un comportamiento similar al del SOAT en Colombia ya que segun el periódico El Tiempo el descuento del 50% del SOAT “lejos de contribuir a reducir la evasión de la póliza, estimular su compra y contribuir de alguna manera a bajar la accidentalidad vial, está dejando un millonario hueco en los recursos del sector de la salud a cargo de la Adres”. Esto nos demuestra que la demanda del soat es inelástica porque como dice el artículo a pesar del descuento en el precio la demanda no se incrementó.
         """
     )
     return
@@ -352,57 +466,170 @@ def __(mo):
 
     Encontramos el equilibrio igualando $Q_o = Q_d$:
 
-
     \begin{equation}
       \begin{split}
         Q_o(p) &= Q_d(p) \\
         p-100 &= 4400-2p \\
         3p &= 4500 \\
-        p &= 1500
+        p* &= 1500
       \end{split}
     \end{equation}
 
+    Lo que quiere decir que la cantidad en equilibrio es:
+
+    \[
+    q* = Q_o(1500) = 1400
+    \]
+
     ## 2.b. Fijar precio máximo
 
-    Para entender el impacto de un precio máximo podemos 
+    Para entender el impacto de un precio máximo ($1200) podemos:
+
+     1. Comparar este precio con el de equilibrio($1500), para notar que este precio máximo impedirá al mercado quilibrarse en el precio ideal.
+     2. Calcular tanto oferta como demanda en el precio exigido y comparar:
+
+    \begin{equation}
+      \begin{split}
+        Q_o(1200) &\quad Q_d(1200) \\
+        1200 - 100 &\quad 4400-2*1200 \\
+        1100 &\quad 4500 \\
+      \end{split}
+    \end{equation}
+
+    Que evidencia una ineficiencia en forma de exceso de **demanda** con respecto a la oferta, que es causada por la imposición de precios en el mercado.
+
     """)
     return
 
 
 @app.cell
-def __(d, draw_subsidy, np, plt):
-    def draw_planetickets(ax):
-        p = np.range(0,250)
-        o = p-0.1
-        ax.plot(o,p, linestyle=(0, (1, 10)), color='r')
+def __(np):
+    def draw_plane_tic(ax):
+        p = np.arange(0,2500)
+        o = p - 100
+        d = 4400-2*p
+        ax.plot(o,p, linestyle='--', color='r')
+        ax.plot(d,p, linestyle='--', color='b')
+        ax.axvline(1400, linestyle=(0, (1, 10)))
+        ax.axhline(1500, linestyle=(0, (1, 10)))
         
-        ax.plot(o,np.array(p)-30, linestyle='--', marker='o', color='r')
-        ax.axvline(160, linestyle=(0, (1, 10)))
-        ax.axhline(290, linestyle=(0, (1, 10)))
-        
-        ax.plot(d,p, linestyle='--', marker='o', color='b')
-        
-        ax.set_xlim([120,250])
-        ax.set_xticks(np.append(ax.get_xticks(),[160,120,240]))
-        ax.set_ylim([250,310])
-        ax.set_yticks(np.append(ax.get_yticks(),[290,310]))
+        ax.set_xlim([0,2000])
+        ax.set_xticks(np.append(ax.get_xticks(),[]))
+        ax.set_ylim([0,2300])
+        ax.set_yticks(np.append(ax.get_yticks(),[]))
         ax.set_xlabel('Quantity')
         ax.set_ylabel('Price')
-        
-        
-        ax.axvline(200, linestyle='dotted')
-        ax.axhline(270, linestyle='dotted')
-        
-        ax.axhline(300, linestyle='dotted')
-        ax.annotate('q**',xy=(202,252), xytext=(202,252),annotation_clip=False, color='blue')
-        ax.annotate('p*',xy=(122,292), xytext=(122,292),annotation_clip=False, color='blue')
-        ax.annotate('p**',xy=(122,272), xytext=(122,272),annotation_clip=False, color='blue')
-        ax.annotate('q*',xy=(162,252), xytext=(162,252),annotation_clip=False, color='blue')
+        ax.annotate('q*',xy=(1400,22), xytext=(1400,22),annotation_clip=False, color='blue')
+        ax.annotate('p*',xy=(22,1500), xytext=(22,1500),annotation_clip=False, color='blue')
+    return draw_plane_tic,
 
-    _, ax2 = plt.subplots(1,1)
-    draw_subsidy(ax2)
-    ax2
-    return ax2, draw_planetickets
+
+@app.cell
+def __(draw_plane_tic, plt):
+    _, ax7= plt.subplots(1,1)
+    draw_plane_tic(ax7)
+    ax7.axhline(1200)
+    ax7
+    return ax7,
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""
+
+    ## 2.c. Fijar precio mínimo
+
+    Para entender el impacto de un precio máximo ($1800) podemos:
+
+     1. Comparar este precio con el de equilibrio($1800), para notar que este precio máximo impedirá al mercado equilibrarse en el precio ideal.
+     2. Calcular tanto oferta como demanda en el precio exigido y comparar:
+
+
+    \begin{equation}
+      \begin{split}
+        Q_o(1200) &\quad Q_d(1200) \\
+        1800 - 100 &\quad 4400-2*1800 \\
+        1700 &\quad 800 \\
+      \end{split}
+    \end{equation}
+
+    Que evidencia una ineficiencia en forma de exceso de **oferta** con respecto a la demanda, que es causada por la imposición de precios en el mercado
+
+    """)
+    return
+
+
+@app.cell
+def __(draw_plane_tic, plt):
+    _, ax8 = plt.subplots(1,1)
+    draw_plane_tic(ax8)
+    ax8.axhline(1800)
+    ax8
+    return ax8,
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""
+
+    ## 2.e. Impuestos
+
+    Al subirse le precio debido al impuesto, los demandantes ya no van a demandar la misma cantidad, ellos demandarían menos cantidad y la curva de la demanda se desplaza hacea la izquierda, resultando en un nuevo punto de equilibrio en P=1300 y Q=1200
+
+    \begin{equation}
+      \begin{split}
+        Q_o(p) &= Q_d(p+300) \\
+        &= 4400 - 2(p+300) \\
+        &= 4400 - 2p - 600 \\
+        &= 3800 - 2p \\
+        Q_o(p)  &= Q_d^t(p) \\
+        p - 100  &= 3800 - 2p\\
+        3p  &= 3900 \\
+        p^{**} &= 1300
+      \end{split}
+    \end{equation}
+
+    Con lo que obtendremos $q^{**} = Q_d^t(1300) = 3800 - 2*1300 = 1200$
+
+    ## 2.f. Elasticidad
+
+    Utilizando la fórmula de elasticidad para el caso lineal:
+
+    \[
+    \epsilon(p) = p\frac{Q'(p)}{Q(p)} = p\frac{m}{mp+c}
+    \]
+
+    Sabemos que la elasticidad en el punto de equilibrio para la oferta será:
+
+    \[
+    \begin{equation}
+      \begin{split}
+        \epsilon_o(p) &= p\frac{m_o}{m_op+c_o} \\
+        &= \frac{p}{p-100} \\
+        \epsilon_o(p*) &= \frac{1500}{1500-100} = 1.071\\
+      \end{split}
+    \end{equation}
+    \]
+
+    Por lo que podemos decir la función de oferta es elastica al precio en el punto de equilibrio, al igual que en cualquier otro punto pues la elasticidad siempre será levemente mayor a 1, es decir, la oferta siempre será un poco elástica.
+
+    Si revisamos la elasticidad de la demanda podemos ver que la oferta es menos elástica que la demanda en el punto de equilibrio
+    , por lo que **el impuesto recaerá mayormente sobre las firmas**.
+
+
+    \[
+    \begin{equation}
+      \begin{split}
+        \epsilon_d(p) &= p\frac{m_d}{m_dp+c_d} \\
+        &= \frac{-2p}{4400-2p} \\
+        &= -\frac{p}{2200-p} \\
+        \epsilon_d(p*) &= -\frac{1500}{2200-1500} = -2.14\\
+      \end{split}
+    \end{equation}
+    \]
+
+    """)
+    return
 
 
 if __name__ == "__main__":
